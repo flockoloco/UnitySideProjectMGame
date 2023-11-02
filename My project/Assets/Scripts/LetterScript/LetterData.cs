3f196e73072f9a9390b9ab20d1b72843eefa81f6
Arthur Santos <tutus.6@hotmail.com>
2023-11-02 15:29:50 +0000

    Letters and score text
    Letters now spawn and move to the left and are destroyed if pressed correctly or not pressed at all

--- My project/Assets/Scripts/LetterScript/LetterData.cs
+++ My project/Assets/Scripts/LetterScript/LetterData.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LetterData : MonoBehaviour
 {
     private Rigidbody2D rb2;
     [SerializeField]
-    private readonly char[] letter = new char[] { 'A', 'S', 'D' };
+    private readonly string[] letter = new string[] { "A", "S", "D" };
+    private bool isInsideCollider = false;
 
-    public char letterValue { get; private set; }
 
-    private char randomGen()
+    public string letterValue { get; private set; }
+
+    private string randomGen()
     {
         int r = Random.Range(0, letter.Length);
 
@@ -22,10 +25,50 @@ public class LetterData : MonoBehaviour
     {
         rb2 = gameObject.GetComponent<Rigidbody2D>();
         letterValue = randomGen();
+        GetComponentInChildren<TextMeshProUGUI>().text = letterValue;
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            CheckPressedKey(Input.inputString);
+        }
     }
 
     private void FixedUpdate()
     {
         rb2.AddForce(transform.right * -5, ForceMode2D.Impulse);
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        isInsideCollider = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isInsideCollider = false;
+        //Maybe change PRESSED IN SEQUENCE STRIKE system to 0 like in guitar hero?
+    }
+
+    private void CheckPressedKey(string inputString)
+    {
+        if(isInsideCollider)
+        {
+            if(inputString[0].ToString().ToUpper() == letterValue)
+            {
+                Destroy(gameObject);
+                Debug.Log("nice");
+            }
+            //Do Stuff when correct letter pressed
+            else
+            {
+                Debug.Log("Wrong key buddy");
+                Destroy(gameObject);
+            }
+        }
+    }
+
+
 }
--- My project/Assets/Scripts/LetterScript/LetterSpawner.cs
+++ My project/Assets/Scripts/LetterScript/LetterSpawner.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class LetterSpawner : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject letter;
+    [SerializeField]
+    private Transform spawnerInitialPosition;
+
+    private float spawnerTimer;
+    private int lettersToFinishRun;
+
     
+
+    private void Start()
+    {
+        //Default Time to spawn letter without interference of difficulty or time
+        spawnerTimer = 2.2f;
+        //Amount of letters to the level end, can change based on difficulty
+        lettersToFinishRun = 10;
+    }
+
+    private void Update()
+    {
+        //Spawn letter if timer is 0 and there's still letters in the counter
+        while(spawnerTimer <= 0f && lettersToFinishRun > 0)
+        {
+            Instantiate(letter, spawnerInitialPosition);
+            spawnerTimer = 2.2f;
+            lettersToFinishRun--;
+        }
+        //Change based on difficulty over time
+        spawnerTimer -= Time.deltaTime;
+
+    }
 }
